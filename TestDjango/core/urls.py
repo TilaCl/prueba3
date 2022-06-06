e044285460e651ebc9c5499661abbc36e6783cab
RomiiPoblete <101873153+RomiiPoblete@users.noreply.github.com>
2022-06-06 02:48:22 -0400

    Update urls.py

--- TestDjango/core/urls.py
+++ TestDjango/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, carrito, comprar, gatos, login, perros, reestablecer, registro, sobrenosotros, ubicanos
+from .views import index, carrito, comprar, gatos, login, perros, reestablecer, registro, sobrenosotros, ubicanos, perfil, registro, mod_perfil, eliminar_perfil
 
 urlpatterns = [
     path('', index,name="index"),
@@ -7,9 +7,12 @@ urlpatterns = [
     path('comprar', comprar, name="comprar"),
     path('gatos', gatos, name="gatos"),
     path('login', login, name="login"),
+    path('', perfil, name = "perfil"),
+    path('registro.html', registro, name = "registro"),
+    path('mod_perfil/<id>', mod_perfil, name = "mod_perfil"),
+    path('eliminar_perfil/<id>', eliminar_perfil, name = "eliminar_perfil"),
     path('perros', perros, name="perros"),
     path('reestablecer', reestablecer, name="reestablecer"),
-    path('registro', registro, name="registro"),
     path('sobrenosotros', sobrenosotros, name="sobrenosotros"),
     path('ubicanos', ubicanos, name="ubicanos"),
 ]  
