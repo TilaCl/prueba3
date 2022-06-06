c78a8a7c06b46b0459bacb3ca18145ff6c22a5d8
RomiiPoblete <101873153+RomiiPoblete@users.noreply.github.com>
2022-06-06 02:49:52 -0400

    Update views.py

--- TestDjango/core/views.py
+++ TestDjango/core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import FormDatos
+from .forms import FormDatos
 
 # Create your views here.
 
@@ -23,8 +25,40 @@ def login(request):
 def reestablecer(request):
     return render(request, 'core/reestablecer.html')
 
+def perfil(request):
+    datosCliente = FormDatos()
+    datos = {
+        'datosCliente': datosCliente
+    }
+    return render(request, 'core/perfil.html',datos)
+
 def registro(request):
-    return render(request, 'core/registro.html')
+    datos = {
+        'form':FormDatos()
+    }
+    if request.method == 'POST':
+        formulario = FormDatos(request.POST)
+        if formulario.is_valid:
+            formulario.save()
+            datos['mensaje'] = "Registrado correctamente"
+    return render(request, 'core/registro.html', datos)
+
+def mod_perfil(request, id):
+    formulario = FormDatos.objects.get(idRut=id)
+    datos = {
+        'form': FormDatos(instance=formulario)
+    }
+    if request.method == 'POST':
+        formulario = FormDatos(data=request.POST, instance=formulario)
+        if formulario.is_valid:
+            formulario.save()
+            datos['mensaje'] = "Modificado correctamente"
+    return render(request, 'core/mod_perfil.html', datos)
+
+def eliminar_perfil(request, id):
+    formulario = FormDatos.objects.get(idRut=id)
+    formulario.delete()
+    return redirect(to="perfil")
 
 def sobrenosotros(request):
     return render(request, 'core/sobrenosotros.html')
